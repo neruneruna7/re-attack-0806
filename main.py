757b395c30955931c0cc2bc9b6d489fb58052b68
neruneruna7 <neruneruna7@gmail.com>
2025-08-06 16:06:50 +0900

    pytorchの動作確認

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import torch
 def main():
+    # pythochで，metalGPUが使えているか確認する
+    if torch.backends.mps.is_available():
+        print("Metal GPU is available.")
+    else:
+        print("Metal GPU is not available.")
     print("Hello from re-attack-0806!")
 
 
